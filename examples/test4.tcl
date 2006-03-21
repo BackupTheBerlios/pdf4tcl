ed41f4e90305060149abdbd612cbcab0b25d54bc
Peter Spjuth <peter.spjuth@gmail.com>
2006-03-21 12:28:35 +0000

    Rectangles are supposed to be filled.

--- examples/test4.tcl
+++ examples/test4.tcl
@@ -12,7 +12,7 @@ p1 arrow 100 150 300 170 10 15
 p1 qCurve 100 100 200 300 300 100
 p1 setStrokeColor 0 0 0
 p1 setFillColor 0.3 0.6 0.9
-p1 rectangle 400 40 166 166
+p1 rectangle 400 40 166 166 -filled 1
 p1 setFillColor 0 0 0
 p1 drawTextAt 100 200 "linksbündig"
 p1 drawTextAt 100 214 "rechtsbündig \xAC" -align right
@@ -26,12 +26,12 @@ for {set w 0} {$w<360} {incr w 15} {
 p1 circle 0 200 400 130
 p1 setFillColor 1 1 1
 p1 setLineStyle 0.1 5 2
-p1 rectangle 348 288 224 104
+p1 rectangle 348 288 224 104 -filled 1
 p1 setFillColor 0 0 0
 p1 setFont 12 "Times-Italic"
 p1 drawTextBox 350 300 220 100 "Dieser Abschnitt sollte im Blocksatz gesetzt sein.\n\nDie Textbox ist 220 Postscript-Punkte breit. pdf4tcl teilt den Text an Leerzeichen, Zeilenendezeichen und Bindestrichen auf." -align justify
 p1 setFillColor 0.8 0.8 0.8
-p1 rectangle 348 408 224 54
+p1 rectangle 348 408 224 54 -filled 1
 p1 setFillColor 0 0 0
 p1 drawTextBox 350 420 220 50 "Eine links- oder rechtsbündige und auch eine zentrierte Ausrichtung in der Textbox sind ebenfalls möglich." -align right
 p1 addJpeg tcl.jpg 1
@@ -51,7 +51,7 @@ p1 arrow 100 150 300 170 10 15
 p1 qCurve 100 100 200 300 300 100
 p1 setStrokeColor 0 0 0
 p1 setFillColor 0.3 0.6 0.9
-p1 rectangle 400 40 166 166
+p1 rectangle 400 40 166 166 -filled 1
 p1 setFillColor 0 0 0
 p1 drawTextAt 100 200 "linksbündig"
 p1 drawTextAt 100 214 "rechtsbündig \xAC" -align right
@@ -65,12 +65,12 @@ for {set w 0} {$w<360} {incr w 15} {
 p1 circle 0 200 400 130
 p1 setFillColor 1 1 1
 p1 setLineStyle 0.1 5 2
-p1 rectangle 348 288 224 104
+p1 rectangle 348 288 224 104 -filled 1
 p1 setFillColor 0 0 0
 p1 setFont 12 "Times-Italic"
 p1 drawTextBox 350 300 220 100 "Dieser Abschnitt sollte im Blocksatz gesetzt sein.\n\nDie Textbox ist 220 Postscript-Punkte breit. pdf4tcl teilt den Text an Leerzeichen, Zeilenendezeichen und Bindestrichen auf." -align justify
 p1 setFillColor 0.8 0.8 0.8
-p1 rectangle 348 408 224 54
+p1 rectangle 348 408 224 54 -filled 1
 p1 setFillColor 0 0 0
 p1 drawTextBox 350 420 220 50 "Eine links- oder rechtsbündige und auch eine zentrierte Ausrichtung in der Textbox sind ebenfalls möglich." -align right
 p1 putImage 1 20 20 -height 75
