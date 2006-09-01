ba24259437222c2e4c0ef768dafdf17095645831
Peter Spjuth <peter.spjuth@gmail.com>
2006-09-01 16:19:47 +0000

    Added mytest proc.

--- tests/init.tcl
+++ tests/init.tcl
@@ -13,3 +13,42 @@ if {[file exists $tmp/pdf4tcl.tcl_i]} {
     source $tmp/pdf4tcl.tcl_i
 }
 package require pdf4tcl
+
+proc mytest {args} {
+    set pattern [lindex $args end]
+    set args [lrange $args 0 end-1]
+
+    set cmds {}
+    set opts {}
+    set isopt 0
+    foreach arg $args {
+        if {$isopt} {
+            set isopt 0
+            lappend opts $arg
+        } elseif {[string match "-*" $arg]} {
+            set isopt 1
+            lappend opts $arg
+        } else {
+            lappend cmds $arg
+        }
+    }
+    set pdf [eval pdf4tcl::new %AUTO% -orient 0 $opts]
+    $pdf startPage
+    foreach cmd $cmds {
+        eval \$pdf $cmd
+    }
+    set res [$pdf get]
+    $pdf destroy
+
+    regexp {stream.*endstream} $res res
+    regsub -all {\s+} $res " " res
+
+    set pattern *[string trim $pattern]*
+    regsub -all {\s+} $pattern " " pattern
+
+    if {[string match *[string trim $pattern]* $res]} {
+        return 1
+    } else {
+        return $res
+    }
+}
