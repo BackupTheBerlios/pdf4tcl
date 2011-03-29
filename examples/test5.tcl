1052a75e720a37b11b9a91ed8ecca2463c0347b0
Peter Spjuth <peter.spjuth@gmail.com>
2011-03-29 22:20:10 +0000

    Fixed example after angle fix

--- examples/test5.tcl
+++ examples/test5.tcl
@@ -36,9 +36,9 @@ p1 line 5.25i 4i 5.25i 7i
 # --- text at various angles
 p1 setStrokeColor 0 0 0
 for {set ang 0} {$ang <= 90} {incr ang 45} {
-    p1 text "^XX^" -x 3.25i -y 5.5i -align left -angle -$ang
-    p1 text "^XX^" -x 4.25i -y 5.5i -align center -angle -$ang
-    p1 text "^XX^" -x 5.25i -y 5.5i -align right -angle -$ang
+    p1 text "^XX^" -x 3.25i -y 5.5i -align left -angle $ang
+    p1 text "^XX^" -x 4.25i -y 5.5i -align center -angle $ang
+    p1 text "^XX^" -x 5.25i -y 5.5i -align right -angle $ang
 }
 
 p1 write -file test5.pdf
